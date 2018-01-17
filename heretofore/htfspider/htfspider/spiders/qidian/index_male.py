5dc7883be45bf295028395b5e7dca345191735a3
heyao <heyao@haimaqingfan.com>
2018-01-17 16:27:39 +0800

    起点 索引信息修改

--- heretofore/htfspider/htfspider/spiders/qidian/index_male.py
+++ heretofore/htfspider/htfspider/spiders/qidian/index_male.py
@@ -5,7 +5,11 @@ create on 2018-01-11 下午12:23
 author @heyao
 """
 
+import re
+import time
+import datetime
 import cPickle as pickle
+
 from scrapy import Request
 from scrapy_redis.spiders import RedisSpider
 from htfspider.items import BookListItem
@@ -14,10 +18,11 @@ from htfspider.items import BookListItem
 class QidianMaleIndexSpider(RedisSpider):
     name = 'qidian_index'
     redis_key = 'qidian:index'
+    today = datetime.datetime.strptime(time.strftime('%Y-%m-%d'), '%Y-%m-%d')
 
     def make_request_from_data(self, data):
         data = pickle.loads(data)
-        url = data.get('url', None)
+        url = data.pop('data_url', None)
         if url:
             req = Request(
                 url,
@@ -49,6 +54,7 @@ class QidianMaleIndexSpider(RedisSpider):
         xpath_folder_url = '//div[@class="book-information cf"]/div[1]/a/img/@src'
         xpath_title = '//div[@class="book-information cf"]/div[2]/h1/em/text()'
         xpath_author = '//div[@class="book-information cf"]//a[@class="writer"]/text()'
+        xpath_author_id = '//a[@class="writer"]/@href'
         xpath_category = '//div[@class="book-information cf"]//a[@class="red"]/text()'
         xpath_introduction = '//div[@class="book-intro"]/p/text()'
         xpath_sub_category = xpath_category
@@ -58,10 +64,12 @@ class QidianMaleIndexSpider(RedisSpider):
         item['folder_url'] = response.urljoin(response.xpath(xpath_folder_url).extract()[0]).strip()
         item['title'] = response.xpath(xpath_title).extract()[0]
         item['author'] = response.xpath(xpath_author).extract()[0]
+        item['author_id'] = response.xpath(xpath_author_id).extract()[0].split('=')[-1]
         item['category'] = response.xpath(xpath_category).extract()[0]
         item['sub_category'] = response.xpath(xpath_sub_category).extract()[1]
         introduction = response.xpath(xpath_introduction).extract()
         item['introduction'] = '\n'.join(p.replace(u'　', '').strip() for p in introduction)
+        item['chan_id'] = re.compile(r'chanId = (\d+);').search(response.body).group()[9:-1]
         read_url = response.urljoin(response.xpath(u'//a[contains(., "免费试读")]/@href').extract()[0])
         if read_url:
             yield Request(
@@ -76,5 +84,8 @@ class QidianMaleIndexSpider(RedisSpider):
     def parse_published_at(self, response):
         item = response.meta['item']
         published_at = response.xpath('//div[@class="info-list cf"]/ul/li[2]/em/text()').extract()[0]
-        item['published_at'] = published_at.replace('.', '-')
+        item['published_at'] = datetime.datetime.strptime(published_at, '%Y.%m.%d')
+        item['status'] = 1
+        item['created_at'] = self.today
+        item['updated_at'] = self.today
         yield item
