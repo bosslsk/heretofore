515c6675ceb60b0d91bdc0d0ef60ade717b4ac27
heyao <heyao@haimaqingfan.com>
2018-01-25 17:41:10 +0800

    debug: code不为0时，直接弃掉该书 add: 抓取过的作品，不再解析

--- heretofore/htfspider/htfspider/spiders/chuangshi/index.py
+++ heretofore/htfspider/htfspider/spiders/chuangshi/index.py
@@ -9,7 +9,9 @@ import json
 from datetime import datetime
 import cPickle as pickle
 
+import pymongo
 from scrapy import Request
+from scrapy.conf import settings
 from scrapy_redis.spiders import RedisSpider
 
 from htfspider.items import BookListItem
@@ -20,7 +22,19 @@ class ChuangshiIndexSpider(RedisSpider):
     redis_key = 'chuangshi:index'
     today = datetime.strptime(time.strftime('%Y-%m-%d'), '%Y-%m-%d')
 
-    def make_requests_from_url(self, data):
+    def __init__(self):
+        super(ChuangshiIndexSpider, self).__init__()
+        mongo_uri = settings.get("MONGO_URI")
+        db_name = settings.get("DB_NAME")
+        auth = settings.get("AUTH")
+        client = pymongo.MongoClient(mongo_uri)
+        db = client[db_name]
+        if auth:
+            db.authenticate(**auth)
+        books = set(i['book_id'] for i in db['book_index'].find({'source_id': 1}, {'book_id': 1}))
+        self.books = books
+
+    def make_request_from_data(self, data):
         data = pickle.loads(data)
         url = data.pop('data_url', None)
         if url:
@@ -35,6 +49,9 @@ class ChuangshiIndexSpider(RedisSpider):
         elements = response.xpath('//div[@class="leftlist"]/table/tr')[1:]
         for element in elements:
             url = response.urljoin(element.xpath('./td/a/@href').extract()[1])
+            book_id = url.split('/')[-1].split('.')[0]
+            if book_id in self.books:
+                continue
             yield Request(
                 url,
                 callback=self.parse_detail,
@@ -77,6 +94,8 @@ class ChuangshiIndexSpider(RedisSpider):
     def parse_published_at(self, response):
         item = response.meta['item']
         published_json = json.loads(response.body)
+        if published_json['code']:
+            return
         published_at = published_json['introinfo']['book']['publishtime'].split(' ')[0]
         item['published_at'] = datetime.strptime(published_at, '%Y-%m-%d')
         yield item
